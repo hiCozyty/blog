a81e1d83940643a22030e3835c9ba3fb9e6941a6
cozyty <cozyty99@gmail.com>
2025-11-25 00:03:00 -0500

    change

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,41 @@
 import adapter from "@sveltejs/adapter-cloudflare";
+import { mdsvex } from "mdsvex";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
+	extensions: [".svelte", ".md"],
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				"*",
+				"/api/posts/page/*",
+				"/blog/category/*/page/",
+				"/blog/category/*/page/*",
+				"/blog/category/page/",
+				"/blog/category/page/*",
+				"/blog/page/",
+				"/blog/page/*",
+			],
+		},
+	},
+
+	preprocess: [
+		mdsvex({
+			// The default mdsvex extension is .svx; this overrides that.
+			extensions: [".md"],
+
+			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+			rehypePlugins: [
+				rehypeSlug,
+				rehypeAutolinkHeadings,
+			],
+		}),
+	],
 };
 
 export default config;
